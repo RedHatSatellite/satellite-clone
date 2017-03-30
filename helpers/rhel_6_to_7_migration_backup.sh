0c52893ddea202c5c5ce6c724db206a0072632a0
sthirugn <sthirugn@redhat.com>
2017-03-30 11:58:29 -0400

    Fix permission issues and refactor postgresql_dump helper script (#161)
    * Fix permission issues and refactor postgresql_dump helper script
    
    Fixes #159

---
+++ helpers/rhel_6_to_7_migration_backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script to create backup files for RHEL6 to 7 migration.
+#
+# Notes:
+# 1. This script runs katello-backup and also creates foreman.dump and candlepin.dump files from your Satellite6 server.
+# 2. Run this script as root user in Satellite6 server.
+# 3. This script stops the katello-service in Satellite.  Please plan accordingly for the outage.
+
+set -x
+
+# Create backup folder
+export backup_dir="/backup"
+mkdir $backup_dir
+
+# Run katello-backup
+katello-backup $backup_dir
+
+# Create foreman and candlepin dumps
+katello-service stop
+service postgresql start
+# Open up write access to let postgres user create dump files
+chmod o+w $backup_dir
+runuser - postgres -c "pg_dump -Fc foreman > "$backup_dir"/foreman.dump"
+runuser - postgres -c "pg_dump -Fc candlepin > "$backup_dir"/candlepin.dump"
+chmod o-w $backup_dir
+katello-service start
+
+set +x
+
+echo "Backup is created in $backup_dir folder. Now copy the generated"\
+     "files to the destination host:"
+echo "config_files.tar.gz, mongo_data.tar.gz, pulp_data.tar,"\
+     "candlepin.dump, foreman.dump"
+echo "Note: Do not copy the generated pgsql_data.tar.gz as this is not"\
+     "required for RHEL6 to 7 migration process."
