336906e86a56ab8f7eed9561578ef8bc6e75d44c
John Mitsch <jomitsch@redhat.com>
2017-05-01 13:17:34 -0400

    Don't show capsule disassociate message when there are no capsules fixes #185

--- roles/satellite-clone/files/disassociate_capsules.rb
+++ roles/satellite-clone/files/disassociate_capsules.rb
@@ -3,25 +3,30 @@ username = "admin"
 password = "changeme"
 external_capsules = []
 external_capsule_ids = `hammer -u #{username} -p #{password} --csv capsule list --search 'feature = \"Pulp Node\"' | tail -n+2 | awk -F, {'print $1'}`
-external_capsule_ids.split("\n").each do |id|
-  lifecycle_environment = `hammer -u #{username} -p #{password} --csv capsule content lifecycle-environments --id #{id} | tail -n+2 | awk -F, {'print $2'}`.split("\n")
-  name = `hammer -u #{username} -p #{password} --csv capsule info --id #{id} | tail -n+2 | awk -F, {'print $2'}`.chomp
-  organization = `hammer -u #{username} -p #{password} --csv capsule info --id 2 | tail -n+2 | awk -F, '{print $(NF-2)}'`.chomp
-  external_capsules << {:id => id, :name => name, :lifecycle_environments => lifecycle_environment, :organization => organization}
-end
-
-reverse_commands = []
-external_capsules.each do |capsule|
-  capsule[:lifecycle_environments].each do |env|
-    `hammer -u #{username} -p #{password} --csv capsule content remove-lifecycle-environment --id #{capsule[:id]} --environment #{env} --organization "#{capsule[:organization]}"`
-    reverse_command = "hammer -u #{username} -p #{password} --csv capsule content add-lifecycle-environment --id #{capsule[:id]} --environment #{env} --organization \"#{capsule[:organization]}\""
-    reverse_commands << reverse_command
+if external_capsule_ids.empty?
+  STDOUT.puts "There are no external capsules to disassociate."
+else
+  external_capsule_ids.split("\n").each do |id|
+    lifecycle_environment = `hammer -u #{username} -p #{password} --csv capsule content lifecycle-environments --id #{id} | tail -n+2 | awk -F, {'print $2'}`.split("\n")
+    name = `hammer -u #{username} -p #{password} --csv capsule info --id #{id} | tail -n+2 | awk -F, {'print $2'}`.chomp
+    organization = `hammer -u #{username} -p #{password} --csv capsule info --id #{id}| tail -n+2 | awk -F, '{print $(NF-2)}'`.chomp
+    external_capsules << {:id => id, :name => name, :lifecycle_environments => lifecycle_environment, :organization => organization}
   end
-end
 
-STDOUT.puts "All Capsules are unassociated with any lifecycle environments. This is to avoid any syncing errors with your original Satellite " \
-            "and any interference with existing infrastructure. To reverse these changes, run the following commands," \
-            " making sure to replace the credentials with your own."
-reverse_commands.each do |reverse|
-  STDOUT.puts reverse
+
+  reverse_commands = []
+  external_capsules.each do |capsule|
+    capsule[:lifecycle_environments].each do |env|
+      `hammer -u #{username} -p #{password} --csv capsule content remove-lifecycle-environment --id #{capsule[:id]} --environment #{env} --organization "#{capsule[:organization]}"`
+      reverse_command = "hammer -u #{username} -p #{password} --csv capsule content add-lifecycle-environment --id #{capsule[:id]} --environment #{env} --organization \"#{capsule[:organization]}\""
+      reverse_commands << reverse_command
+    end
+  end
+
+  STDOUT.puts "All Capsules are unassociated with any lifecycle environments. This is to avoid any syncing errors with your original Satellite " \
+              "and any interference with existing infrastructure. To reverse these changes, run the following commands," \
+              " making sure to replace the credentials with your own."
+  reverse_commands.each do |reverse|
+    STDOUT.puts reverse
+  end
 end
