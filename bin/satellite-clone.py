18817a24ecce701a2df43defd65527c985c5e070
John Mitsch <jomitsch@redhat.com>
2017-11-07 10:32:45 -0500

    review changes

--- bin/satellite-clone.py
+++ bin/satellite-clone.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+import argparse
 import os
 import subprocess
+import sys
 
 def yes_or_no(question):
     while "the answer is invalid":
@@ -10,71 +11,55 @@ def yes_or_no(question):
         if reply[0] == 'y':
             return True
         if reply[0] == 'n':
-            exit()
-
-def tail(f, n, offset=0):
-  offset_total = str(n+offset)
-  stdin,stdout = os.popen2("tail -n "+offset_total+" "+f)
-  stdin.close()
-  lines = stdout.readlines(); stdout.close()
-  return lines[:,-offset]
+            exit(1)
 
 def main():
-    parser = OptionParser(usage="Usage: satellite-clone [options]\n" \
-				"Example: satellite-clone --start-at-task=\"Clean yum info\"")
-    parser.add_option("-v", "--verbose",
+    parser = argparse.ArgumentParser(usage="Usage: satellite-clone [options]\n" \
+                                           "Example: satellite-clone --start-at-task=\"Clean yum info\"")
+    parser.add_argument("-v", "--verbose",
                       action="store_true",
                       dest="verbose",
                       default=False,
                       help="verbose output")
-    parser.add_option("--start-at-task",
+    parser.add_argument("--start-at-task",
                       action="store", # optional because action defaults to "store"
                       dest="start_at_task",
                       help="Start at a specific task",)
-    parser.add_option("-i", "--interactive",
-                      action="store_true",
-                      dest="interactive",
-                      default=False,
-                      help="interactive mode with prompt for each task")
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
 
     DEFAULT_PRODUCTION_INSTALL_PATH = "/usr/share/satellite-clone"
-    DEFAULT_PLAYBOOK_FILE = DEFAULT_PRODUCTION_INSTALL_PATH + "/satellite-clone-playbook.yml"
+    DEFAULT_PLAYBOOK_FILE = os.path.join(DEFAULT_PRODUCTION_INSTALL_PATH, "satellite-clone-playbook.yml")
 
     yes_or_no("This will initiate satellite-clone. Do you want to proceed?")
  
     ansible_args = [] 
-    if options.verbose:
+    if args.verbose:
         ansible_args.append("-vvv")
-    if options.start_at_task:
-        ansible_args.append("--start-at-task \"{0}\"".format(options.start_at_task))
-    if options.interactive:
-        ansible_args.append("--interactive")
+    if args.start_at_task:
+        ansible_args.append("--start-at-task")
+        ansible_args.append(args.start_at_task)
 
     if not os.path.isdir(DEFAULT_PRODUCTION_INSTALL_PATH):
-         print("It looks like satellite-clone has not been installed properly, " \
-               "{0} does not exist.".format(DEFAULT_PRODUCTION_INSTALL_PATH))
-         exit()
+         sys.stdout.write("It looks like satellite-clone has not been installed properly, " \
+                          "{0} does not exist.".format(DEFAULT_PRODUCTION_INSTALL_PATH))
+         exit(1)
 
-    os.environ["ANSIBLE_CONFIG"] = "{0}/ansible.production.cfg".format(DEFAULT_PRODUCTION_INSTALL_PATH)
+    os.environ["ANSIBLE_CONFIG"] = os.path.join(DEFAULT_PRODUCTION_INSTALL_PATH, 'ansible.production.cfg')
 
-    inventory_path = "{0}/inventory".format(DEFAULT_PRODUCTION_INSTALL_PATH)
-    ansible_playbook = "ansible-playbook -i {0} {1} {2}".format(inventory_path, 
-                                                                 " ".join(ansible_args),
-                                                                 DEFAULT_PLAYBOOK_FILE)
+    inventory_path = os.path.join(DEFAULT_PRODUCTION_INSTALL_PATH, 'inventory')
+    ansible_playbook = ["ansible-playbook", "-i", inventory_path] + ansible_args + [DEFAULT_PLAYBOOK_FILE]
  
-    print(ansible_playbook)
+    sys.stdout.write(" ".join(ansible_playbook))
     run_playbook = subprocess.Popen(ansible_playbook, 
-                                    shell=True, 
                                     stdout=subprocess.PIPE, 
                                     bufsize=1,
-                                    cwd="{0}".format(DEFAULT_PRODUCTION_INSTALL_PATH))
+                                    cwd=DEFAULT_PRODUCTION_INSTALL_PATH)
 
     # provide real-time output
     while True:
         line = run_playbook.stdout.readline()
-        print(line)
+        sys.stdout.write(line)
         if not line: break
     run_playbook.stdout.close()
     run_playbook.wait()
